31255b7da009f66023a90d834b2879907cbcfb1d
Anatolii Burtsev <ab.canada.world@gmail.com>
2023-11-18 23:21:09 -0800

    add basic AUTH to post method

--- lib/simple_apigw_lambda_ts-stack.ts
+++ lib/simple_apigw_lambda_ts-stack.ts
@@ -8,7 +8,6 @@ export class SimpleApigwLambdaTsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Define the Lambda functions
     const getStockLambda = new lambda.Function(this, 'GetStockHandler', {
       runtime: lambda.Runtime.NODEJS_18_X,
       code: lambda.Code.fromAsset('lambda/'),
@@ -21,7 +20,6 @@ export class SimpleApigwLambdaTsStack extends cdk.Stack {
       handler: 'postOrder.handler'
     });
 
-    // Define the API Gateway
     const api = new apigateway.RestApi(this, 'Endpoint', {
       restApiName: 'SimpleAPITS'
     });
@@ -30,11 +28,37 @@ export class SimpleApigwLambdaTsStack extends cdk.Stack {
     const postOrderIntegration = new apigateway.LambdaIntegration(postOrderLambda);
 
     api.root.resourceForPath('stock').addMethod('GET', getStockIntegration);
-    api.root.resourceForPath('order').addMethod('POST', postOrderIntegration);
+
+    // Add auth to post order API
+    const apiKey = new apigateway.ApiKey(this, 'PostOrderApiKey', {
+      enabled: true,
+    });
+
+    const usagePlan = new apigateway.UsagePlan(this, 'UsagePlan', {
+      name: 'BasicUsagePlan',
+      throttle: {
+        rateLimit: 10,
+        burstLimit: 2
+      }
+    });
+
+    usagePlan.addApiKey(apiKey);
+    usagePlan.addApiStage({
+      stage: api.deploymentStage,
+      api: api
+    });
+
+    api.root.resourceForPath('order').addMethod('POST', postOrderIntegration,
+      {apiKeyRequired: true});
 
     new cdk.CfnOutput(this, 'ApiUrl', {
       value: api.url,
       description: 'The URL of the API Gateway',
+    });
+
+    new cdk.CfnOutput(this, 'ApiKey', {
+      value: apiKey.keyId,
+      description: 'API Key Id for accessing the POST method'
     });
   }
 }
